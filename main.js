6097f80334e6da0bef3ed8c5919c117a975a9d74
DivineMatongo <dstanley3247@gmail.com>
2026-04-05 03:57:39 +0200

    Ensure grid cells are perfectly square

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const grid = document.querySelector("#grid");
+let grid = document.querySelector("#grid");
+
+function sizeGrid() {
+    const smallerDimension = Math.min(window.innerHeight, window.innerWidth);
+    grid.style.width = grid.style.height = `${smallerDimension}px`;
+}
 
 function createGrid(size) {
+    sizeGrid();
     for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
         for (let ii = 0; ii < size; ii++) {
